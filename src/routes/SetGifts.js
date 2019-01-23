74e7de5dd62265703a4c4da13d17068eaf198647
douxiaochen <1976325920@qq.com>
2019-01-23 23:58:24 +0800

    后台更新礼物内容

--- src/routes/SetGifts.js
+++ src/routes/SetGifts.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'dva';
 import { createForm } from 'rc-form';
-import { Form, Icon, Input, Button,message, Checkbox,Row,Col,InputNumber,Select,Table,Pagination } from 'antd';
+import { Form, Icon, Input, Button,message, Checkbox,Row,Col,InputNumber,Select,Table,Pagination,Modal } from 'antd';
 import styles from '../styles/IndexPage.less';
 import *as programHost from '../utils/ajax';
 var store = require('store');
@@ -12,27 +12,29 @@ var store = require('store');
 
 export default class SetGifts extends React.Component {
 	constructor(props) {
-  super(props);
-  this.state={
-  a:1,
-  giftImg:'http://img3.tuwandata.com/uploads/play/1142101545745926.png',
-  giftMeili:1,
-  giftName:'',
-  giftPrice:1,
-  giftRemark:'',
-  giftNumber:0,
-  giftTypeArr:[
-      {id:1,label:'普通礼物'},
-      {id:2,label:'冠名礼物'},
-      {id:3,label:'活动礼物'},
-  ],
-  giftType:{id:1,label:'普通礼物'},
-  giftList:[],
-  colums:[],
-  currentPage:1,
-   totalPage:0,
-   editing:false,
-  }
+    super(props);
+    this.state={
+        a:1,
+        giftImg:'http://img3.tuwandata.com/uploads/play/1142101545745926.png',
+        giftMeili:1,
+        giftName:'',
+        giftPrice:1,
+        giftRemark:'',
+        giftNumber:0,
+        giftTypeArr:[
+            {id:1,label:'普通礼物'},
+            {id:2,label:'冠名礼物'},
+            {id:3,label:'活动礼物'},
+            // {id:4,label:'礼物'},
+        ],
+        giftType:{id:1,label:'普通礼物'},
+        giftList:[],
+        colums:[],
+        currentPage:1,
+        totalPage:0,
+        editing:false,
+        giftId:'',
+    }
       
  }
 	componentWillMount(){
@@ -122,7 +124,6 @@ export default class SetGifts extends React.Component {
     }
     console.log(sbdata);
     const content=this;
-    console.log(programHost.getAuth('/admin/add/gift/info'));
     fetch(`${programHost.APIhost}/admin/add/gift/info`, {
         method: 'POST',
         dataType: 'json',
@@ -138,7 +139,7 @@ export default class SetGifts extends React.Component {
         console.log(response);
         response.json().then((res) => {
             console.log(res);
-           if(res.statusCode===107){ message.success(res.message);content.emptyGift();}
+           if(res.statusCode===107){ message.success(res.message);content.emptyGift();content.componentWillMount();}
            else{message.warn(res.message);}
             },(data) => {
             console.log(data)
@@ -158,11 +159,14 @@ export default class SetGifts extends React.Component {
   }
   editGift(record){
     console.log(record);
-    const {giftTypeArr}=this.state;
-    let giftType={};
-    giftTypeArr.map((item,index)=>{
-        if(item._id===record._id){giftType={id:record.type,label:item.label};}
-    })
+    // const {giftTypeArr}=this.state;
+    // let giftType={};
+    // giftTypeArr.map((item,index)=>{
+    //     if(item.id===record.type){
+    //         giftType.id=record.type;
+    //         giftType.label=item.label;
+    //     }
+    // })
     this.setState({
         editing:true,
         giftImg:record.icon,
@@ -171,48 +175,122 @@ export default class SetGifts extends React.Component {
         giftPrice:record.price,
         giftRemark:record.note,
         giftNumber:record.codeNum,
-        giftType,
+        // giftType,
+        giftId:record._id,
     })
 }
   deleteGift(record){
       console.log(record);
+      const content = this;
+      fetch(`${programHost.APIhost}/admin/del/gift/info/${record._id}`, {
+        method: 'DELETE',
+        dataType: 'json',
+        // body:JSON.stringify(sbdata),
+        mode: 'cors',
+        credentials: 'include',
+        headers: new Headers({
+            Accept: 'application/json',
+            'Content-Type': 'application/json;charset=UTF-8',
+            'Authorization':programHost.getAuth(`/admin/del/gift/info/${record._id}`),// 获取token
+        }),
+        }).then((response) => {
+        console.log(response);
+        response.json().then((res) => {
+            console.log(res);
+           if(res.statusCode===107){
+                message.success(res.message);
+                content.componentWillMount();
+            }
+           else{message.warn(res.message);}
+            },(data) => {
+            console.log(data)
+        });
+    });
   }
-  
-//   changePage (page){
-//     console.log(page);
-//     const {totalPage}=this.state;
-//     if(page>=totalPage){this.setState({currentPage:totalPage});}
-//     else{ this.setState({currentPage:page});}
-//     const content=this;
-//     const sbdata={where:{}};
-//             //联网
-//         fetch(`${programHost.APIhost}/user/anchor/game/list/${page}/10`, {
-//             method: 'POST',
-//             dataType: 'json',
-//             body:JSON.stringify(sbdata),
-//             mode: 'cors',
-//             credentials: 'include',
-//             headers: new Headers({
-//                 Accept: 'application/json',
-//                 'Content-Type': 'application/json;charset=UTF-8',
-//                 'Authorization':programHost.getAuth(`/user/anchor/game/list/${page}/10`),// 获取token
-//             }),
-//             }).then((response) => {
-//             console.log(response);
-//             response.json().then((res) => {
-//                 console.log(res);
-//                 if(res.statusCode===107){
-//                     const arr=res.resource;
-//                     arr.forEach((item,ind)=>{item.key=ind;});
-//                     content.setState({totalPage:res.sum,gameApplyList:arr});
-//                 }
-//                 },(data) => {
-//                 console.log(data)
-//             });
-//         });
-//   }
+    handleOk(){
+        const {giftImg,giftName,giftPrice,giftRemark,giftNumber,giftMeili,giftType,giftId}=this.state;
+        const sbdata={
+            name: giftName,
+            icon:giftImg,
+            price:giftPrice,
+            meiliValue:giftMeili,
+            // giftType:giftType.id,
+            // type: 1,
+            codeNum:giftNumber,
+            note:giftRemark,
+            state:1,
+        }
+        console.log(sbdata);
+        const content=this;
+        fetch(`${programHost.APIhost}/admin/get/gift/info/${giftId}`, {
+            method: 'PUT',
+            dataType: 'json',
+            body:JSON.stringify(sbdata),
+            mode: 'cors',
+            credentials: 'include',
+            headers: new Headers({
+                Accept: 'application/json',
+                'Content-Type': 'application/json;charset=UTF-8',
+                'Authorization':programHost.getAuth(`/admin/get/gift/info/${giftId}`),// 获取token
+            }),
+            }).then((response) => {
+            console.log(response);
+            response.json().then((res) => {
+                console.log(res);
+            if(res.statusCode===107){ message.success(res.message);content.emptyGift();content.componentWillMount();content.setState({editing:false})}
+            else{message.warn(res.message);}
+                },(data) => {
+                console.log(data)
+            });
+        });
+    }
+  handleCancel(){
+      this.setState({
+        editing:false,
+        giftImg:'http://img3.tuwandata.com/uploads/play/1142101545745926.png',
+        giftMeili:1,
+        giftName:'',
+        giftPrice:1,
+        giftRemark:'',
+        giftNumber:0,
+        giftType:{id:1,label:'普通礼物'},
+      })
+  }
+  changePage (page){
+    console.log(page);
+    const {totalPage}=this.state;
+    if(page>=totalPage){this.setState({currentPage:totalPage});}
+    else{ this.setState({currentPage:page});}
+    const content=this;
+    const sbdata={where:{}};
+            //联网
+        fetch(`${programHost.APIhost}/admin/add/gift/info`, {
+            method: 'POST',
+            dataType: 'json',
+            body:JSON.stringify(sbdata),
+            mode: 'cors',
+            credentials: 'include',
+            headers: new Headers({
+                Accept: 'application/json',
+                'Content-Type': 'application/json;charset=UTF-8',
+                'Authorization':programHost.getAuth(`/admin/add/gift/info`),// 获取token
+            }),
+            }).then((response) => {
+            console.log(response);
+            response.json().then((res) => {
+                console.log(res);
+                if(res.statusCode===107){
+                    const arr=res.resource;
+                    arr.forEach((item,ind)=>{item.key=ind;});
+                    content.setState({totalPage:res.sum,gameApplyList:arr});
+                }
+                },(data) => {
+                console.log(data)
+            });
+        });
+  }
     render() {
-        const {giftImg,giftName,giftPrice,giftRemark,giftNumber,giftMeili,giftTypeArr,giftType,currentPage,totalPage,colums,giftList}=this.state;
+        const {editing,giftImg,giftName,giftPrice,giftRemark,giftNumber,giftMeili,giftTypeArr,giftType,currentPage,totalPage,colums,giftList}=this.state;
         const Option = Select.Option;
         return ( <div className={styles.normal}>
                 <h1 className={styles.title}>添加礼物</h1>
@@ -270,6 +348,55 @@ export default class SetGifts extends React.Component {
                     <Table columns={colums} dataSource={giftList} pagination={false} />
                     <Pagination current={currentPage} onChange={(page)=>{this.changePage(page);}} total={totalPage}/>
                 </div>
+                <Modal
+                    title="编辑礼物"
+                    okText='确认'
+                    cancelText='取消'
+                    width={500}
+                    visible={editing}
+                    onOk={()=>{this.handleOk()}}
+                    onCancel={()=>{this.handleCancel()}}
+                >
+
+                    <div className={styles.formItem0}>
+                        <Row>
+                            <Col span={8}>礼物图片：</Col>
+                            <Col span={8}><img style={{width:50,height:50}} src={giftImg} alt=""/></Col>
+                            <Col span={8}></Col>
+                        </Row>
+                    </div>
+                    <div className={styles.formItem}>
+                        <Row>
+                            <Col span={8}>礼物名称：</Col>
+                            <Col span={10}><Input placeholder="请填写礼物名称" value={giftName}onChange={(e)=>{this.setState({giftName:e.target.value})}}  /></Col>
+                        </Row>
+                    </div>
+                    <div className={styles.formItem}>
+                        <Row>
+                            <Col span={8}>礼物序号：</Col>
+                            <Col span={10}><InputNumber style={{width:'100%'}} min={0} value={giftNumber} onChange={(value)=>{this.setState({giftNumber:value})}} /></Col>
+                        </Row>
+                    </div>
+                    <div className={styles.formItem}>
+                        <Row>
+                            <Col span={8}>礼物价格（钻石）：</Col>
+                            <Col span={10}><InputNumber style={{width:'100%'}} min={0} value={giftPrice} onChange={(value)=>{this.setState({giftPrice:value})}} /></Col>
+                        </Row>
+                    </div>
+                    <div className={styles.formItem}>
+                        <Row>
+                            <Col span={8}>导师获得魅力值：</Col>
+                            <Col span={10}><InputNumber style={{width:'100%'}} min={1}  placeholder="导师收到此礼物可增加的魅力值"  value={giftMeili}onChange={(value)=>{this.setState({giftMeili:value})}}  /></Col>
+                        </Row>
+                    </div>
+                    
+                    <div className={styles.formItem}>
+                        <Row>
+                            <Col span={8}>礼物简介：</Col>
+                            <Col span={10}><Input placeholder="请填写礼物介绍" value={giftRemark}onChange={(e)=>{this.setState({giftRemark:e.target.value})}}  /></Col>
+                        </Row>
+                    </div>
+                </Modal>       
             </div>
         )
     }
